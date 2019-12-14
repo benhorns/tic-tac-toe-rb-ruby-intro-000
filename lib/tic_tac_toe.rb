f6b8c582d082d8ae22ba62168c3f52fbd010c8d4
Benjamin Horns <benhorns@gmail.com>
2019-12-14 15:49:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,3 +5,25 @@ else board[0]=="X" || board[0]=="O"
   true
 end
 end
+
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+def turn_count(board)
+  turns = 0
+  board.each do |counter|
+    if counter == "X" || counter == "O"
+      turns += 1
+    end
+  end
+  return turns
+end
+
+def current_player(board)
+  turn_count(board)%2 == 0 ? (return "X") : (return "O")
+end
